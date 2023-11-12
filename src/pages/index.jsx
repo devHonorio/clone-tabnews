18255b6eab87d095bd8d52db955a9f18d2b8f4a6
devHonorio <honorio.dev@gmail.com>
2023-11-12 01:47:58 -0300

    Adiciona frase de motivação e propósito

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,10 @@
-const Home = () => <h1>O Palmeiras não tem mundial</h1>;
+const Home = () => (
+  <h1>
+    Local para pessoas brutalmente exatas e extremamente empáticas
+    <strong> simultaneamente</strong>, um espaço para discução de ideias,
+    duvidas e soluções para problemas corriqueiros da área de assistencia
+    tecnica de smartphones, computadores e impressoras.
+  </h1>
+)
 
-export default Home;
+export default Home

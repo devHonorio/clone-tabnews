c0cafb114b893c7866e9baab0cd37e1dad7bda0d
devHonorio <honorio.dev@gmail.com>
2023-12-19 14:34:20 -0300

    add  config file with database service

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,10 @@
 const Home = () => (
-  <h1>
+  <p>
     Local para pessoas brutalmente exatas e extremamente empáticas
     <strong> simultaneamente</strong>, um espaço para discução de ideias,
     duvidas e soluções para problemas corriqueiros da área de assistencia
     tecnica de smartphones, computadores e impressoras.
-  </h1>
+  </p>
 )
 
 export default Home

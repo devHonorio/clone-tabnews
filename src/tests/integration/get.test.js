296461715ca48c7b2491261655102114ce7aa1e0
devHonorio <honorio.dev@gmail.com>
2024-06-25 23:48:22 -0300

    finsh  endpoint

--- src/tests/integration/get.test.js
+++ src/tests/integration/get.test.js
@@ -2,4 +2,17 @@ test('GET to /api/v1/status should return 200', async () => {
   const response = await fetch('http://localhost:3000/api/v1/status')
 
   expect(response.status).toBe(200)
+
+  const responseBory = await response.json()
+
+  const parsedUpdateAt = new Date(responseBory.update_at).toISOString()
+  expect(responseBory.update_at).toEqual(parsedUpdateAt)
+
+  expect(responseBory.dependencies).toBeDefined()
+
+  expect(responseBory.dependencies.database).toBeDefined()
+
+  expect(responseBory.dependencies.database.version).toBe('16.1')
+  expect(responseBory.dependencies.database.max_connections).toBe(100)
+  expect(responseBory.dependencies.database.opened_connections).toBe(1)
 })

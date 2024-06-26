678e6113f2622d1c0c364d5bf86135fbc8752cb6
devHonorio <honorio.dev@gmail.com>
2024-06-26 00:37:06 -0300

    makes   more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -9,13 +9,21 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
   })
 
-  await client.connect()
+  console.log('Credenciais do Postgres: ', {
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+  })
 
   try {
+    await client.connect()
     const result = await client.query(queryObject)
     return result
   } catch (error) {
     console.log(error)
+    throw error
   } finally {
     await client.end()
   }

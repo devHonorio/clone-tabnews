a5c9996950a8c03ebe74953305b7ec5dfbcb4a6e
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2024-11-08 15:06:18 -0300

    refactor: rename all descriptions to use new pattern

--- src/tests/integration/api/v1/status/get.test.js
+++ src/tests/integration/api/v1/status/get.test.js
@@ -2,21 +2,25 @@ import orchestrator from 'src/tests/orchestrator'
 
 beforeAll(async () => await orchestrator.waitForAllServices())
 
-test('GET to /api/v1/status should return 200', async () => {
-  const response = await fetch('http://localhost:3000/api/v1/status')
+describe('GET to /api/v1/status', () => {
+  describe('Anonymous user', () => {
+    test('Retryven current system status', async () => {
+      const response = await fetch('http://localhost:3000/api/v1/status')
 
-  expect(response.status).toBe(200)
+      expect(response.status).toBe(200)
 
-  const responseBory = await response.json()
+      const responseBory = await response.json()
 
-  const parsedUpdateAt = new Date(responseBory.update_at).toISOString()
-  expect(responseBory.update_at).toEqual(parsedUpdateAt)
+      const parsedUpdateAt = new Date(responseBory.update_at).toISOString()
+      expect(responseBory.update_at).toEqual(parsedUpdateAt)
 
-  expect(responseBory.dependencies).toBeDefined()
+      expect(responseBory.dependencies).toBeDefined()
 
-  expect(responseBory.dependencies.database).toBeDefined()
+      expect(responseBory.dependencies.database).toBeDefined()
 
-  expect(responseBory.dependencies.database.version).toBe('16.1')
-  expect(responseBory.dependencies.database.max_connections).toBe(100)
-  expect(responseBory.dependencies.database.opened_connections).toBe(1)
+      expect(responseBory.dependencies.database.version).toBe('16.1')
+      expect(responseBory.dependencies.database.max_connections).toBe(100)
+      expect(responseBory.dependencies.database.opened_connections).toBe(1)
+    })
+  })
 })
